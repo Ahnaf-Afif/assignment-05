4fb7b54b34d1bbb8dd0e9249d1f12c08510b8f04
Ahnaf-Afif <williamhens123@gmail.com>
2026-03-08 16:03:22 +0600

    completed search issue and modal

--- main.js
+++ main.js
@@ -335,4 +335,87 @@ const count = () => {
   document.getElementById("issueCount").innerText = `${count} Issues`;
 };
 
+const loadSearchProblems = () => {
+  const searchField = document.getElementById("search-issue");
+  const searchText = searchField.value;
+  fetch(
+    `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchText}`,
+  )
+    .then((res) => res.json())
+    .then((data) => searchIssue(data.data));
+};
+
+const searchIssue = (data) => {
+  const problemsBox = document.getElementById("problems-box");
+  problemsBox.innerHTML = "";
+
+  for (const problem of data) {
+    console.log(data);
+    const div = document.createElement("div");
+    div.className = "cursor-pointer";
+    div.onclick = () => {
+      openModal(problem);
+    };
+    div.innerHTML = `<div
+                                    class="max-w-sm bg-white border border-gray-100 ${problem.status === "open" ? "border-t-emerald-500" : "border-t-purple-500"} border-t-4 rounded-xl shadow-sm"
+                                >
+                                    
+
+                                    <div class="p-5">
+                                    <div class="flex justify-between items-start mb-4">
+                                        <div>
+                                            ${problem.status === "open" ? `<img src="/assets/Open-Status.png" alt="" />` : `<img src="/assets/Closed- Status .png" alt="" />`}
+                                        
+                                        
+                                        </div>
+
+                                        <span
+                                        class="px-4 py-1 rounded-full  ${problem.priority === "high" ? "bg-red-50" : problem.priority === "medium" ? "bg-yellow-50" : "bg-green-50"} ${problem.priority === "high" ? "text-red-500" : problem.priority === "medium" ? "text-yellow-500" : "text-green-500"} text-xs font-bold tracking-wider uppercase"
+                                        >
+                                        ${problem.priority}
+                                        </span>
+                                    </div>
+
+                                    <h3 class="text-gray-800 font-bold text-lg leading-tight mb-2">
+                                        ${problem.title}
+                                    </h3>
+                                    <p class="text-gray-500 text-sm leading-relaxed mb-5">
+                                        ${problem.description}
+                                    </p>
+
+                                    <div class="flex gap-2 mb-6">
+                                        
+                                        
+                                            
+                                            <span class="text-sm">${problem.labels
+                                              .map(
+                                                (label) => `
+                                                        <span class="badge badge-info badge-outline text-xs font-bold">
+                                                            ${label}
+                                                        </span>
+                                                    `,
+                                              )
+                                              .join("")}
+                                            </span>
+                                        </div>
+
+                                    </div>
+
+                                    <div class="flex justify-between pt-4 pl-2 pr-2 pb-5 border-t border-gray-100">
+                                        <div>
+                                              <p class="text-gray-500 text-sm font-medium">#${problem.id} by ${problem.author}</p>
+                                        <p class="text-gray-400 text-xs mt-1">${problem.createdAt}</p>
+                                        </div>
+                                        <div>
+                                                 <p class="text-gray-500 text-sm font-medium">Assignee: ${problem.assignee === "" ? "Unassigned" : problem.assignee}</p>
+                                        <p class="text-gray-400 text-xs mt-1">Updated: ${problem.updatedAt}</p>  
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>`;
+    problemsBox.appendChild(div);
+  }
+  count();
+};
+
 loadProblems();
